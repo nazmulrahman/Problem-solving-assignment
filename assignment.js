a02306abd5eee862f8f8792ede86aad17772482d
nazmulrahman <nazmulrahman1658@gmail.com>
2021-01-22 00:13:40 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// kilometerToMeter
+function kilometerToMeter(value) {
+    if (value < 0) { // for negative value
+        value = Math.abs(value);
+        console.log("You entered a negative value. Since distance cannot be negative, it is shown as positive");
+    }
+    var meter = value * 1000;
+    return meter;
+}
+var result = kilometerToMeter(-234);
+console.log(result);
