529a5c2ea795c82a19d9afd84443ae8215d77299
nazmulrahman <nazmulrahman1658@gmail.com>
2021-01-22 12:18:27 +0600

    1,3,4 done

--- assignment.js
+++ assignment.js
@@ -1,4 +1,4 @@
-// kilometerToMeter
+//(1) kilometerToMeter
 function kilometerToMeter(value) {
     if (value < 0) { // for negative value
         value = Math.abs(value);
@@ -6,4 +6,41 @@ function kilometerToMeter(value) {
     }
     var meter = value * 1000;
     return meter;
+}
+
+
+
+//(3) hotelCost
+function hotelCost(days) {
+    var rent = 0;
+    if (days <= 10) {
+        rent = days * 100;
+    } else if (days <= 20) {
+        var firstOffer = 10 * 100;
+        var remaining = days - 10;
+        var secondOffer = remaining * 80;
+        rent = firstOffer + secondOffer;
+    } else {
+        var firstOffer = 10 * 100;
+        var secondOffer = 10 * 80;
+        var remaining = days - 20;
+        var thirdOffer = remaining * 50;
+        rent = firstOffer + secondOffer + thirdOffer;
+    }
+    return rent;
+}
+
+
+
+//(4) megaFriend
+
+function megaFriend(friends) {
+    var bigName = ''; // largest element will store here
+    for (var i = 0; i < friends.length; i++) {
+        var elements = friends[i];
+        if (elements.length > bigName.length) {
+            bigName = elements;
+        }
+    }
+    return bigName;
 }
